20f6a740b5d69cf616d4f0d3e08d7e1f047c9e15
Máximo Galindo <galindomaximo0@gmail.com>
2025-06-16 20:58:54 -0300

    missing change

--- PabellonFrontEnd/src/environment/environment.prod.ts
+++ PabellonFrontEnd/src/environment/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://vps-5076192-x.dattaweb.com/api',
-  fileUrl: 'http://vps-5076192-x.dattaweb.com'
+  apiUrl: 'https://pabellonweb.com/api',
+  fileUrl: 'https://pabellonweb.com'
 };
